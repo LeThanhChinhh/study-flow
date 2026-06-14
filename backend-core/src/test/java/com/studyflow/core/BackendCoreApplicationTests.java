357278ffa3197c7b1b9f73b2bb28fe804391b4a9
Lê Thành Chính <bulletz6105@gmail.com>
2026-06-14 10:43:22 +0700

    setup base project

---
+++ backend-core/src/test/java/com/studyflow/core/BackendCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyflow.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
